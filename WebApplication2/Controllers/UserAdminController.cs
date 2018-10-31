f63399a60f3099e063a9fe1d326a6d521fa161bf
超 刘 <greg_114@163.com>
2018-10-31 09:31:33 +0800

    UserAdmin的Edit Action

--- WebApplication2/Controllers/UserAdminController.cs
+++ WebApplication2/Controllers/UserAdminController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,6 +31,83 @@ namespace WebApplication2.Controllers
 
 
 
-  
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                throw new ApplicationException();
+            }
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                throw new ApplicationException();
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            return View(new EditUserViewModel()
+            {
+                Id = user.Id,
+                Email = user.Email,
+
+                RolesList = _roleManager.Roles.ToList().Select(x => new SelectListItem()
+                {
+                    Selected = userRoles.Contains(x.Name),
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(EditUserViewModel editUser, params string[] selectedRole)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(editUser.Id);
+                if (user == null)
+                {
+                    throw new ApplicationException();
+                }
+
+                user.UserName = editUser.Email;
+                user.Email = editUser.Email;
+
+
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                selectedRole = selectedRole ?? new string[] { };
+
+                var result = await _userManager.AddToRolesAsync(user, selectedRole.Except(userRoles).ToArray<string>());
+
+                if (!result.Succeeded)
+                {
+                    AddErrors(result);
+                    return View();
+                }
+                result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRole).ToArray<string>());
+
+                if (!result.Succeeded)
+                {
+                    AddErrors(result);
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Something failed.");
+            return View();
+        }
+
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
     }
 }
