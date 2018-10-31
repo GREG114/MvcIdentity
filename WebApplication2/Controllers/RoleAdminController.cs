94a0848a27bd0935266a3e0b65b6c75db20c970e
超 刘 <greg_114@163.com>
2018-10-31 09:25:18 +0800

    增加了新建角色的action和视图，能用

--- WebApplication2/Controllers/RoleAdminController.cs
+++ WebApplication2/Controllers/RoleAdminController.cs
@@ -26,5 +26,34 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
+
+        // POST: /Roles/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(IdentityRole roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole(roleViewModel.Name);
+
+                var roleresult = await _roleManager.CreateAsync(role);
+                if (!roleresult.Succeeded)
+                {
+                    AddErrors(roleresult);
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        //不知道是个干什么的，报错用的？
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
     }
 }
