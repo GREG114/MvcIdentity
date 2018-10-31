bacd4f7c989cf08c7b942aa792b0633915d8789f
超 刘 <greg_114@163.com>
2018-10-31 09:04:50 +0800

    UserAdmin控制器和视图

---
+++ WebApplication2/Controllers/UserAdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public UserAdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await _userManager.Users.ToListAsync());
+        }
+    }
+}
