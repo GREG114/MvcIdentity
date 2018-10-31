dec070380757de5059cc55d7c625e5500b4d80f2
超 刘 <greg_114@163.com>
2018-10-31 09:08:17 +0800

    RoleAdmin 视图和控制器基本

---
+++ WebApplication2/Controllers/RoleAdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class RoleAdminController: Controller
+    {
+
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public RoleAdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public ActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+    }
+}
